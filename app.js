12eb9c0e430c6ca7edf0fc45eb592c745603e661
Santoro Pinto <joao.santoro.pinto@accenture.com>
2019-08-01 11:28:10 -0300

    setting port by Heroku

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
-app.listen(3000, () => {
+var port = process.env.PORT || 3000;
+app.listen(port, () => {
  console.log("Server running on port 3000");
 });
 
