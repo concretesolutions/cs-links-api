be4f194ce116aa180d62e9506446c04cf199a50b
Santoro Pinto <joao.santoro.pinto@accenture.com>
2019-08-01 13:44:48 -0300

    changing some key words and default

--- app.js
+++ app.js
@@ -26,12 +26,11 @@ app.get("/links", (req, res, next) => {
             res.send('https://blog.accenture.com/concrete/2018/08/06/salario-beneficios/');
             break;
         case 'todos':
+        default:
             res.send('Guia de Sobrevivência - https://blog.accenture.com/concrete/ \n'+
                       'Valores - https://blog.accenture.com/concrete/2018/08/01/valores/ \n'+
                       'Salário e Benefícios - https://blog.accenture.com/concrete/2018/08/06/salario-beneficios/ \n' +
                       'Viagens - https://blog.accenture.com/concrete/2018/08/06/viagens/');
-        default:
-            res.send('https://portal.accenture.com');
             break;
     }
 
