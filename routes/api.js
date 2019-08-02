a41cfd758bb9aa8a906ea4047e6d76d41497c647
Santoro Pinto <joao.santoro.pinto@accenture.com>
2019-08-02 15:29:33 -0300

    removing console.log

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,6 @@ module.exports = {
         let text = req.query.text;
         let user = req.query.user_name;
         let msg = messageBuilder(user, text);
-        console.log(msg);
         res.send(msg);
     }
 };
