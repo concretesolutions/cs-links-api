530cdb44d8dca8d6e60c552d6c1bbfdea708dda1
Joao Felipe Santoro Pinto <joao.felipe@concrete.com.br>
2019-08-16 22:39:38 -0300

    linter issues

--- helper/utils.js
+++ helper/utils.js
@@ -1,11 +1,11 @@
 const guiaLink = 'Guia de Sobrevivência - https://blog.accenture.com/concrete/';
 const valoresLink = 'Valores - https://blog.accenture.com/concrete/2018/08/01/valores/';
 const salariosLink = 'Salário e Benefícios - https://blog.accenture.com/concrete/2018/08/06/salario-beneficios/';
-const materiaisLink = 'Materiais Padrão - https://blog.accenture.com/concrete/2018/09/21/materiais-padrao/'
+const materiaisLink = 'Materiais Padrão - https://blog.accenture.com/concrete/2018/09/21/materiais-padrao/';
 const viagensLink = 'Viagens - https://blog.accenture.com/concrete/2018/08/06/viagens/';
 const linkSelector = function(keyWord) {
 
-  if(!keyWord || keyWord.match(/^todos$/)) {
+  if (!keyWord || keyWord.match(/^todos$/)) {
     return 'Essa key word não está mapeada, '+
           'mas seguem todos os links que temos mapeados no momento\n' +
           guiaLink + '\n' +
@@ -14,19 +14,21 @@ const linkSelector = function(keyWord) {
           materiaisLink + '\n' +
           viagensLink;
   }
-  if(keyWord.match(/^guia$/)) {
+  if (keyWord.match(/^guia$/)) {
     return guiaLink;
   }
-  if(keyWord.match(/^valores$/)) {
+  if (keyWord.match(/^valores$/)) {
     return valoresLink;
   }
-  if(keyWord.match(/^viagem|viagens$/)) {
+  if (keyWord.match(/^viagem|viagens$/)) {
     return viagensLink;
   }
-  if(keyWord.match(/^material|materiais|padrão|padrao|apresentação|apresentacao$/)) {
+  if (keyWord
+      .match(/^material|materiais|padrão|padrao|apresentação|apresentacao$/)) {
     return materiaisLink;
   }
-  if(keyWord.match(/^benefício|beneficio|benefícios|beneficios|salário|salario$/)) {
+  if (keyWord
+      .match(/^benefício|beneficio|benefícios|beneficios|salário|salario$/)) {
     return salariosLink;
   }
 };
