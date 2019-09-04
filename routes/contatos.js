44ed9e3e34f486218a44ce50f55666f75f646e1f
Joao Felipe Santoro Pinto <joao.felipe@concrete.com.br>
2019-09-04 16:35:39 -0300

    Update contatos.js
    fixing typo

--- routes/contatos.js
+++ routes/contatos.js
@@ -7,7 +7,7 @@ const contacts = '* Contas a Pagar (11) 5188-1851 \n'
                + '  - (11) 5188-3244\n'
                + '  - (21) 4501-9244\n'
                + '  - (41) 3014-2980\n'
-               + '* It´s Seg (Planos de Saúde & Dental \n'
+               + '* It´s Seg (Planos de Saúde & Dental) \n'
                + '  - (11) 5188-1908 \n'
                + '  - atendimento.accenture@itsseg.com';
 
