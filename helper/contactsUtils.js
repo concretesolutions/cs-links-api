e7434a1b7f36e52309668f525ed63de7ad0d3d32
cs-camilo-micheletto <camilo.m.silva@accenture.com>
2020-10-27 10:18:37 -0300

    config: added eslint npm script and build configuration

--- helper/contactsUtils.js
+++ helper/contactsUtils.js
@@ -1,36 +1,36 @@
-const itemSelector = require("./matchItem");
+const itemSelector = require('./matchItem');
 
 const list = {
   contas: {
-    action:`Contas a Pagar - ${process.env.CONTACT_CONTAS}`,
+    action: `Contas a Pagar - ${process.env.CONTACT_CONTAS}`,
     regex: /^contas$/,
   },
   cio: {
-    action:`CIO - ${process.env.CONTACT_CIO}`,
+    action: `CIO - ${process.env.CONTACT_CIO}`,
     regex: /^CIO|cio$/,
   },
   dp: {
-    action:`Departamento Pessoal - ${process.env.CONTACT_DP}`,
+    action: `Departamento Pessoal - ${process.env.CONTACT_DP}`,
     regex: /^(departamento )?pessoal|dp$/,
   },
   myTe: {
-    action:`Atendimento Time & Expenses (10 às 16h) - ${process.env.CONTACT_MYTE}`,
+    action: `Atendimento Time & Expenses (10 às 16h) - ${process.env.CONTACT_MYTE}`,
     regex: /^myte|expenses$/,
   },
   asoc: {
-    action:`ASOC - ${process.env.CONTACT_ASOC}`,
+    action: `ASOC - ${process.env.CONTACT_ASOC}`,
     regex: /^asoc$/,
   },
   itSeg: {
-    action:`It´s Seg - ${process.env.CONTACT_ITSEG}`,
+    action: `It´s Seg - ${process.env.CONTACT_ITSEG}`,
     regex: /^itseg|itSeg|planos$/,
-  }
+  },
 };
 
-const env = "contatos";
+const env = 'contatos';
 
 module.exports = {
-  messageBuilder: function(keyWord) {
+  messageBuilder(keyWord) {
     const greeting = 'Olá, seguem os contatos:\n';
     const contactFounds = itemSelector(keyWord, list, env);
     return greeting + contactFounds;
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-const {links} = require('./routes/links');
-const {contacts} = require('./routes/contacts');
+const { links } = require('./routes/links');
+const { contacts } = require('./routes/contacts');
 
 app.listen(port, () => {
-  console.log('Server running on port ' + port);
+  console.log(`Server running on port ${port}`); // eslint-disable-line no-console
 });
 
 app.get('/links', links);
